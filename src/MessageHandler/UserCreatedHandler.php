b1ff02c235c26abd93135cff07afe9b0d2edcfb7
deltamemories <maxenot123@gmail.com>
2026-03-31 19:14:09 +0200

    feat(user): add handler for UserCreated event

---
+++ src/MessageHandler/UserCreatedHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\MessageHandler;
+
+
+use App\Message\PostCreatedMessage;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class PostCreatedHandler {
+    public function __construct(
+        private LoggerInterface $logger
+    )
+    {}
+
+    public function __invoke(
+        PostCreatedMessage $message
+    )
+    {
+        $this->logger->info("Entity User created with id: {$message->postId}");
+    }
+}
