99df2ab62701bb7970e30ad1d6374ac7ee43c62b
deltamemories <maxenot123@gmail.com>
2026-04-05 14:08:02 +0200

    chore(documentation): install twig asset

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
